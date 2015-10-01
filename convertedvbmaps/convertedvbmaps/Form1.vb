258364cf3fa4b3605c103562f5717034d8ce16d0
max <maxwellfundi@live.com>
2015-10-01 11:40:35 +0300

    Polygontrial

--- convertedvbmaps/convertedvbmaps/Form1.vb
+++ convertedvbmaps/convertedvbmaps/Form1.vb
@@ -2,6 +2,7 @@
 Imports GMap.NET.WindowsForms
 Imports GMap.NET.WindowsForms.Markers
 
+
 Public Class Form1
     Private Sub GMapControl1_Load(sender As Object, e As EventArgs) Handles GMapControl1.Load
         ' Initialize map
@@ -13,9 +14,8 @@ Public Class Form1
         ' Placing a marker on a certain position
         Dim markersOverlay = New GMapOverlay("markers")
         Dim markerone = New GMarkerGoogle(New PointLatLng(1, 0), GMarkerGoogleType.green)
-        Dim markertwo = New GMarkerGoogle(New PointLatLng(0.07, 1), GMarkerGoogleType.blue)
+        Dim markertwo = New GMarkerGoogle(New PointLatLng(16, 16), GMarkerGoogleType.blue)
         Dim markerthree = New GMarkerGoogle(New PointLatLng(10, 10), GMarkerGoogleType.yellow)
-
 
         markersOverlay.Markers.Add(markerone)
         markersOverlay.Markers.Add(markertwo)
@@ -24,18 +24,24 @@ Public Class Form1
 
 
 
-
-        ' Add new code here
+        'polygon trial 
+        Dim polyOverlay As New GMapOverlay("polygons")
+        Dim points As IList(Of PointLatLng) = New List(Of PointLatLng)()
+        points.Add(New PointLatLng(-25.969562, 32.585789))
+        points.Add(New PointLatLng(-25.966205, 32.588171))
+        points.Add(New PointLatLng(-25.968134, 32.591647))
+        points.Add(New PointLatLng(-25.971684, 32.589759))
+        Dim polygon As New GMapPolygon(points, "mypolygon")
+        polygon.Fill = New SolidBrush(Color.FromArgb(50, Color.Red))
+        polygon.Stroke = New Pen(Color.Red, 1)
+        polyOverlay.Polygons.Add(polygon)
+        GMapControl1.Overlays.Add(polyOverlay)
 
     End Sub
+    Private Sub GmapControl1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles GMapControl1.MouseDoubleClick
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        GMapControl1.Zoom = 2
     End Sub
 End Class
-
-
-
-
 
 
